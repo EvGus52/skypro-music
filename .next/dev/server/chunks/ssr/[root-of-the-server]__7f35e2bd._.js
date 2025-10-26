946cab73eb2f1cd6d90e51f23b39b1e720497d19
EvGus52 <evgenijguskov247@gmail.com>
2025-10-26 21:33:01 +0300

    подключил шрифты, добавил картинки

--- .next/dev/server/chunks/ssr/[root-of-the-server]__7f35e2bd._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__7f35e2bd._.js
@@ -69,7 +69,7 @@ function Home() {
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                             children: [
                                 "Looking for a starting point or more instructions? Head over to",
-                                " ",
+                                ' ',
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
                                     href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app",
                                     target: "_blank",
@@ -80,9 +80,9 @@ function Home() {
                                     lineNumber: 20,
                                     columnNumber: 13
                                 }, this),
-                                " ",
+                                ' ',
                                 "or the",
-                                " ",
+                                ' ',
                                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
                                     href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app",
                                     target: "_blank",
@@ -93,7 +93,7 @@ function Home() {
                                     lineNumber: 28,
                                     columnNumber: 13
                                 }, this),
-                                " ",
+                                ' ',
                                 "center."
                             ]
                         }, void 0, true, {
