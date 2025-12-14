22124b538e545f753525b732bbda04b6a83ee5fc
EvGus52 <evgenijguskov247@gmail.com>
2025-12-14 18:01:08 +0300

    реалиовал логику комбинированой фильтрации

--- .next/dev/static/chunks/src_app_layout_tsx_469ed6a6._.js
+++ .next/dev/static/chunks/src_app_layout_tsx_469ed6a6._.js
@@ -3,7 +3,7 @@
     chunks: [
   "static/chunks/[root-of-the-server]__79f78277._.css",
   "static/chunks/node_modules_5bf0f18d._.js",
-  "static/chunks/src_store_0276d70b._.js"
+  "static/chunks/src_12a86a1a._.js"
 ],
     source: "dynamic"
 });
